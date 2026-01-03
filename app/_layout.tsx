31c85211628ebac0c1c5d4460e32e002878e1d7d
Aakif-Ullah-Khan <khanaakif205@gmail.com>
2026-01-03 15:46:37 +0500

    added glustach ui

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,18 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "react-native";
 
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import '@/global.css';
+
 export default function RootLayout() {
   return (
-    <Stack>
+    
+    <GluestackUIProvider mode="dark">
+      <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <StatusBar backgroundColor="white" barStyle="dark-content" />
     </Stack>
+    </GluestackUIProvider>
+  
   );
 }
--- components/productcard.tsx
+++ components/productcard.tsx
@@ -3,8 +3,8 @@ import { Text, View } from "react-native";
 export default function Product({ item  }:any) {
   return (
     <View>
-      <Text>{item.name}</Text>
       <Text>{item.id}</Text>
+      <Text>{item.name}</Text>
       <Text>{item.description}</Text>
       <Text>{item.price}</Text>
       <Text>{"\n"}</Text>
